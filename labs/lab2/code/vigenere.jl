ff8bbabefa0d7aadf1463e790a7212237369f80a
alexfleeex <alexfleeex@DESKTOP-4OMV1HN>
2025-09-26 13:00:43 +0300

    completed lab2

---
+++ labs/lab2/code/vigenere.jl
@@ -0,0 +1,28 @@
+function vigenere_encrypt(text, key)
+        alphabet = 'a':'z'
+        output = ""
+        key_index = 1
+
+        for i in text
+                if isletter(i)
+                        offset = findfirst(isequal(key[key_index]), alphabet) - 1
+                        index = findfirst(isequal(i), alphabet) + offset
+                        index > 26 && (index -= 26)
+                        output *= alphabet[index]
+                        key_index += 1
+                        key_index > length(key) && (key_index = 1)
+                else
+                        output *= i
+                end
+        end
+
+        return output
+end
+
+# text = "hello world"
+# key = "key"
+
+text = ARGS[1]
+key = ARGS[2]
+
+println("Зашифрованный текст: ", vigenere_encrypt(text, key), ".")
